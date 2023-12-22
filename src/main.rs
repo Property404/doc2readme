664b939418b0f0defe749dd2b2127a8a41866480
Property404 <git@dagans.dev>
2023-12-22 10:04:33 -0700

    Add failing test

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,13 @@ use anyhow::{anyhow, Result};
 use html2md::TagHandlerFactory;
 use schmargs::{ArgsWithHelp, Schmargs};
 use scraper::{Html, Selector};
-use std::{collections::HashMap, fs, path::PathBuf, str};
+use std::{
+    collections::HashMap,
+    fs::{self, File},
+    io::Write,
+    path::PathBuf,
+    str,
+};
 
 #[derive(Debug, Schmargs)]
 #[schmargs(iterates_over=String)]
@@ -14,6 +20,9 @@ struct BareArgs {
     /// Base URL for relative links
     #[arg(short, long)]
     base_url: Option<String>,
+    /// Output path
+    #[arg(short, long)]
+    output: Option<String>,
     /// Path to the file
     path: PathBuf,
 }
@@ -45,7 +54,14 @@ fn main() -> Result<()> {
         }),
     );
 
-    println!("{}", html2md::parse_html_custom(&docblock, &handlers));
+    let markdown = html2md::parse_html_custom(&docblock, &handlers);
+
+    if let Some(output_file) = args.output {
+        let mut file = File::create(output_file)?;
+        file.write_all(markdown.as_bytes())?;
+    } else {
+        println!("{}", markdown);
+    }
 
     Ok(())
 }
---
+++ tests/test.rs
@@ -0,0 +1,42 @@
+use anyhow::Result;
+use pretty_assertions::assert_eq;
+use std::{env, fs, path::Path, process::Command};
+use tempfile::TempDir;
+
+#[test]
+fn integration_test() -> Result<()> {
+    let paths = fs::read_dir("assets/readmes/").unwrap();
+
+    for path in paths {
+        let path = path?;
+        test_project(path.path())?;
+    }
+
+    Ok(())
+}
+
+fn test_project(markdown_file: impl AsRef<Path>) -> Result<()> {
+    // Create cargo project
+    let cargo_dir = TempDir::new()?;
+    let copy_options = Default::default();
+    fs_extra::dir::copy("assets/bare/", cargo_dir.path(), &copy_options)?;
+    let cargo_dir = cargo_dir.path().join("bare");
+    fs::copy(markdown_file.as_ref(), cargo_dir.join("README.md"))?;
+    env::set_current_dir(cargo_dir)?;
+
+    // Build documentation
+    assert!(Command::new("cargo").arg("doc").output()?.status.success());
+
+    // Extract docs
+    assert_cmd::Command::cargo_bin("doc2readme")?
+        .arg("target/doc/bare/index.html")
+        .arg("-o")
+        .arg("EXTRACTED.md")
+        .ok()?;
+
+    let original_text = fs::read_to_string("README.md")?;
+    let extracted_text = fs::read_to_string("EXTRACTED.md")?;
+
+    assert_eq!(original_text, extracted_text);
+    Ok(())
+}
